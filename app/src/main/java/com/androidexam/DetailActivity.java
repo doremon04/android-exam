b367ae18509a20094a18a6d933283c74ffcb2c1d
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-05-26 14:39:11 +0700

    Update create, edit, delete

--- app/src/main/java/com/androidexam/DetailActivity.java
+++ app/src/main/java/com/androidexam/DetailActivity.java
@@ -7,21 +7,46 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.androidexam.dao.IStudentDAO;
+import com.androidexam.dao.StudentDAOImpl;
+import com.androidexam.databinding.ActivityDetailBinding;
+import com.androidexam.entity.Student;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class DetailActivity extends AppCompatActivity {
+    private ActivityDetailBinding binding;
+    private int mID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        int idS = getIntent().getExtras().getInt("ids");
+
+        if (idS > 0) {
+            loadStudent(idS);
+            binding.btnAdd.setEnabled(false);
+            binding.btnEdit.setOnClickListener(view -> update());
+            binding.btnEdit.setEnabled(true);
+            binding.btnDelete.setEnabled(true);
+            return;
+        }
+
+        binding.btnAdd.setOnClickListener(view -> create());
+        binding.btnAdd.setEnabled(true);
+        binding.btnEdit.setEnabled(false);
+        binding.btnDelete.setEnabled(false);
     }
 
     public void openDatePicker(View view) {
@@ -59,5 +84,77 @@ public class DetailActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loadStudent(int idS) {
+        IStudentDAO studentDAO = new StudentDAOImpl(this);
+        Student c = studentDAO.selectById(idS);
+        mID = c.getId();
+
+        binding.edtName.setText(c.getName());
+//        binding.subjectsSpinner.s(c.getClassName());
+        binding.edtPhone.setText(c.getPhone());
+        binding.edtEmail.setText(c.getEmail());
+        binding.edtBirthday.setText(c.getBirthday().toString());
+        binding.radMale.setChecked(c.getGender());
+    }
+
+    private void create() {
+        String name = binding.edtName.getText().toString();
+        String phone = binding.edtPhone.getText().toString();
+        String email = binding.edtEmail.getText().toString();
+        String className = binding.subjectsSpinner.getSelectedItem().toString();
+        String birthdayStr = binding.edtBirthday.getText().toString();
+        boolean gender = binding.radMale.isChecked();
+
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date birthday = null;
+        try {
+            birthday = sdf.parse(birthdayStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Student s = new Student(name, className, gender, phone, birthday, email);
+        IStudentDAO studentDAO = new StudentDAOImpl(this);
+        boolean result = studentDAO.insert(s);
+        if (result) {
+            Toast.makeText(this, "Add new successfully", Toast.LENGTH_SHORT).show();
+
+            // go to main activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else
+            Toast.makeText(this, "Add new error", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void update() {
+        String name = binding.edtName.getText().toString();
+        String phone = binding.edtPhone.getText().toString();
+        String email = binding.edtEmail.getText().toString();
+        String className = binding.subjectsSpinner.getSelectedItem().toString();
+        String birthdayStr = binding.edtBirthday.getText().toString();
+        boolean gender = binding.radMale.isChecked();
+
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date birthday = null;
+        try {
+            birthday = sdf.parse(birthdayStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Student s = new Student(mID, name, className, gender, phone, birthday, email);
+        IStudentDAO studentDAO = new StudentDAOImpl(this);
+        boolean result = studentDAO.insert(s);
+        if (result) {
+            Toast.makeText(this, "Update successfully", Toast.LENGTH_SHORT).show();
+
+            // go to main activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else
+            Toast.makeText(this, "Update error", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/androidexam/dao/StudentDAOImpl.java
+++ app/src/main/java/com/androidexam/dao/StudentDAOImpl.java
@@ -32,22 +32,48 @@ public class StudentDAOImpl implements IStudentDAO {
             @SuppressLint("Range") String name = c.getString(c.getColumnIndex("name"));
             @SuppressLint("Range") String className = c.getString(c.getColumnIndex("className"));
             @SuppressLint("Range") boolean gender = c.getInt(c.getColumnIndex("gender")) > 0;
-//            @SuppressLint("Range") String birthdayStr = c.getString(c.getColumnIndex("birthday"));
+            @SuppressLint("Range") String birthdayStr = c.getString(c.getColumnIndex("birthday"));
             @SuppressLint("Range") String phone = c.getString(c.getColumnIndex("phone"));
             @SuppressLint("Range") String email = c.getString(c.getColumnIndex("email"));
 
-//            @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             Date birthday = null;
-//            try {
-//                birthday = sdf.parse(birthdayStr);
-//            } catch (ParseException e) {
-//                e.printStackTrace();
-//            }
+            try {
+                birthday = sdf.parse(birthdayStr);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
 
             Student s = new Student(id, name, className, gender, phone, birthday, email);
             list.add(s);
         }
         return list;
+    }
+
+    @Override
+    public Student selectById(int id) {
+        String sql = "SELECT * FROM tblStudent WHERE id = ?";
+        Cursor c = mDB.rawQuery(sql, new String[]{String.valueOf(id)});
+        while (c.moveToNext()) {
+            @SuppressLint("Range") String name = c.getString(c.getColumnIndex("name"));
+            @SuppressLint("Range") String className = c.getString(c.getColumnIndex("className"));
+            @SuppressLint("Range") boolean gender = c.getInt(c.getColumnIndex("gender")) > 0;
+            @SuppressLint("Range") String birthdayStr = c.getString(c.getColumnIndex("birthday"));
+            @SuppressLint("Range") String phone = c.getString(c.getColumnIndex("phone"));
+            @SuppressLint("Range") String email = c.getString(c.getColumnIndex("email"));
+
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date birthday = null;
+            try {
+                birthday = sdf.parse(birthdayStr);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            Student s = new Student(id, name, className, gender, phone, birthday, email);
+            return s;
+        }
+        return null;
     }
 
     @Override
